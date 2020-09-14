7593eb350816f5bea4b97a8c7891f9649e6fa358
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-15 00:48:03 +0530

    Add header to home

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+  const [navMenu, setNavMenu] = React.useState('topnav');
+
+  const toggleMenu = () => {
+    if (navMenu === '') {
+      setNavMenu('toggled');
+    } else {
+      setNavMenu('');
+    }
+  };
+
+  return (
+    <div>
+      <div className={`top-menu ${navMenu}`}>
+        <div className='top-menu-lead'>
+          <img src='/logo512.png' width={100} height={50} />
+          <h3>plus infinity labs</h3>
+        </div>
+
+        <div className='right'>
+          <div className='top-menu-item'>About</div>
+          <div className='top-menu-item'>Contact</div>
+        </div>
+        <FontAwesomeIcon
+          icon={faBars}
+          className='top-menu-icon'
+          onClick={toggleMenu}
+        />
+        <div className='clear-fix' />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,9 @@
-import React from "react";
-
+import React from 'react';
+import Header from '../../components/Header/Header';
 const Home = () => {
   return (
     <div>
+      <Header />
       <p>Hello world</p>
     </div>
   );
