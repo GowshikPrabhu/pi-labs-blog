d9b8a7e61713d0070583506de3c5a8b0c670b53f
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-16 20:47:21 +0530

    Add search to home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Landing from './components/Landing/Landing';
+import Toolbar from './components/Toolbar/Toolbar';
+import './Home.css';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Landing />
+      <div className='home-layout'>
+        <div className='layout-container'>
+          {/* Blog cards */}
+          <p>Blog posts</p>
+        </div>
+        <div className='layout-container toolbar'>
+          {/* Search and filter */}
+          <Toolbar />
+        </div>
+      </div>
     </div>
   );
 };
