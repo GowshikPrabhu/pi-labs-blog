bfe6ae82c07cff63e2cc3764b596e02c225ec6f5
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-22 12:18:36 +0530

    Create blog card

--- src/pages/Articles/Articles.js
+++ src/pages/Articles/Articles.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Articles.css';
 import * as bgimg from '../../assets/images/img-1.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Articles = () => {
   return (
@@ -10,8 +9,17 @@ const Articles = () => {
         <div className='article-heading'>featured posts</div>
         <div className='divider'></div>
         <div className='article-body'>
-          <div className='max-image-card'>
-            <img src={bgimg} />
+          <div className='max-img-card' onClick={() => {}}>
+            <img src={bgimg} alt='article-bg' />
+            <div className='full-overlay'>
+              <div className='card-tag-area'>
+                <div className='card-tag'>Algorithm</div>
+                <div className='card-tag'>D and C</div>
+              </div>
+              <div className='card-in-title'>
+                <p>Divide and conquer algorithms implementaion with c++</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
