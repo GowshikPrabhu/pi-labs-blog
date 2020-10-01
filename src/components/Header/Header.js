3c55ef7944728c4b604ea3ce5e4b86bf22e1bf9d
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-01 18:22:48 +0530

    create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [navMenu, setNavMenu] = React.useState('topnav');
+  const [searchValue, setSearchValue] = useState('');
+  const [theme, setTheme] = useState('light');
 
   const toggleMenu = () => {
     if (navMenu === '') {
@@ -13,27 +16,68 @@ const Header = () => {
       setNavMenu('');
     }
   };
+  const onChangeSearch = (e) => setSearchValue(e.target.value);
+
+  const toggleTheme = () => {
+    if (theme == 'light') {
+      setTheme('dark');
+    } else if (theme == 'dark') {
+      setTheme('light');
+    }
+  };
 
   return (
-    <div className='header-top'>
-      <div className={`top-menu ${navMenu}`}>
-        <div className='top-menu-lead'>
-          <img src='/logo512.png' alt='logo' width={100} height={50} />
-          <h3>plus infinity labs</h3>
+    <nav>
+      <div className='nav-inner'>
+        <div className='nav-left'>
+          <img src='/logo512.png' alt='logo' />
+          <p>plus infinity labs</p>
+          <ul>
+            <li>
+              <Link to='/' className='nav-link'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/blogs' className='nav-link'>
+                Blogs
+              </Link>
+            </li>
+            <li>
+              <Link to='/about' className='nav-link'>
+                About
+              </Link>
+            </li>
+          </ul>
         </div>
-
-        <div className='right'>
-          <div className='top-menu-item'>About</div>
-          <div className='top-menu-item'>Contact</div>
+        <div className='nav-right'>
+          <div class='search-container'>
+            <input
+              type='text'
+              placeholder='search'
+              value={searchValue}
+              onChange={onChangeSearch}
+              className='search-input'
+              name='search-bar'
+              onSubmit={() => console.log(searchValue)}
+            />
+          </div>
+          {theme == 'light' ? (
+            <FontAwesomeIcon
+              icon={faMoon}
+              className='nav-theme-toggle'
+              onClick={toggleTheme}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faSun}
+              className='nav-theme-toggle'
+              onClick={toggleTheme}
+            />
+          )}
         </div>
-        <FontAwesomeIcon
-          icon={faBars}
-          className='top-menu-icon'
-          onClick={toggleMenu}
-        />
-        <div className='clear-fix' />
       </div>
-    </div>
+    </nav>
   );
 };
 
