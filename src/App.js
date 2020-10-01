8edd6057c167c45a9ee403c5f7fa8c91d25a9c1e
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-01 10:37:23 +0530

    Setup layout with routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from "react";
-import "./App.css";
-import Home from "./pages/Home/Home";
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import About from './pages/About';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <Home />;
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/blogs'>
+            <Blogs />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
