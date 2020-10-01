372911ec0c95b41fb3d0c698310f72a229f8601d
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-01 14:39:03 +0530

    Create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
+/** Footer component */
 const Footer = () => {
   return (
     <footer>
@@ -14,7 +15,23 @@ const Footer = () => {
           <FontAwesomeIcon icon={faYoutube} className='footer-icon' />
         </div>
         <div className='footer-right-links'>
-          <span>Home</span>•<span>Blogs</span>•<span>About</span>
+          <span>
+            <Link to='/' className='footer-link'>
+              Home
+            </Link>
+          </span>
+          •
+          <span>
+            <Link to='/blogs' className='footer-link'>
+              Blogs
+            </Link>
+          </span>
+          •
+          <span>
+            <Link to='/about' className='footer-link'>
+              About
+            </Link>
+          </span>
         </div>
       </div>
     </footer>
