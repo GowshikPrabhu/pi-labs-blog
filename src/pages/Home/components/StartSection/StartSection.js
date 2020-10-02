1da0495210dc31754b1ab34811c4110a46d0877c
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-02 22:42:03 +0530

    Create home page start area

---
+++ src/pages/Home/components/StartSection/StartSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StartSection.css';
+
+const StartSection = () => {
+  return (
+    <div className='home-start'>
+      <p>Think, Do, Share and Repeat</p>
+    </div>
+  );
+};
+
+export default StartSection;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import StartSection from './components/StartSection/StartSection';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <StartSection />
     </div>
   );
 };
