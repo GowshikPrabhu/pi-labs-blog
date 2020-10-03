976843e76251f2b135d213dd6a475b0d87511552
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-03 19:11:27 +0530

    add all cards to home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,17 @@ const Home = () => {
     <div>
       <StartSection />
       <div className='main-section'>
-        <BlogCard viewMode='vertical' />
+        <BlogCard viewMode='horizontal' id={1} />
+        <div className='multi-cards set-1'>
+          <BlogCard viewMode='vertical' id={2} />
+          <BlogCard viewMode='vertical' id={3} />
+          <BlogCard viewMode='vertical' id={4} />
+        </div>
+        <div className='multi-cards'>
+          <BlogCard viewMode='vertical' id={5} />
+          <BlogCard viewMode='vertical' id={6} />
+          <BlogCard viewMode='vertical' id={7} />
+        </div>
       </div>
     </div>
   );
