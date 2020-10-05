ca65beda0a2ca4dfb9f1fe23a79519266e58a67c
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-05 09:35:20 +0530

    Create about us page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './index.css';
+import AboutDetails from './components/AboutDetails';
+
+const data = [
+  {
+    title: 'Who we are?',
+    description:
+      'Hello and welcome to Plus Infinity Labs. We are team based in Indid. We do freelance application developing and content writing.'
+  },
+  {
+    title: 'Our goal',
+    description:
+      'We like to share our knowledge and experience with others. Our goal is to share how simple and fun programming is.'
+  }
+];
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='aboutus'>
+      {data.map(({ title, description }) => (
+        <AboutDetails title={title} description={description} />
+      ))}
     </div>
   );
 };
