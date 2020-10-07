a81eb78eb29db2cff697caea33c055686ff3bc76
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-07 09:10:37 +0530

    add start image to read blog

--- src/pages/ReadBlog/ReadBlog.js
+++ src/pages/ReadBlog/ReadBlog.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import BlogHeader from './components/BlogHeader/BlogHeader';
 import './ReadBlog.css';
+import img1 from '../../assets/images/img-1.jpg';
 
 const ReadBlog = () => {
   return (
-    <div>
+    <div className='read-blog'>
       <BlogHeader />
+      <div className='start-img'>
+        <img src={img1} alt='start img' width='80%' height='100%' />
+      </div>
     </div>
   );
 };
