479ceae564381c007a87ff813400a5c93c2a7b73
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-13 20:09:00 +0000

    create start file

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// Main server
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+const PORT = process.env.PORT | 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running MODE:${process.env.NODE_ENV} PORT:${PORT}`)
+);
