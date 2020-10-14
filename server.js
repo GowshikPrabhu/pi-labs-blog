1ac51f1cf2db6bc782fc1539234b6b6b3effb994
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-14 21:58:49 +0530

    add and setup dotenv file

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Setup dotenv file for development
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('common'));
+}
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running MODE:${process.env.NODE_ENV} PORT:${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
