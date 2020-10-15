38e8277ad760613c16a0250d7a371e07eb67b2d1
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-15 14:06:09 +0530

    create methods to add blog and get all blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const Blog = require('../models/Blog');
 const asyncHandler = require('../middlewares/async');
+const ErrorResponse = require('../utils/error');
 
 exports.getBlogs = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, data: {} });
+  res.status(200).json(res.advancedResults);
 });
 
 exports.getBlog = asyncHandler(async (req, res, next) => {
@@ -14,7 +15,9 @@ exports.getFeaturedBlogs = asyncHandler(async (req, res, next) => {
 });
 
 exports.createBlog = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true, data: {} });
+  const blog = await Blog.create(req.body);
+
+  res.status(201).json({ success: true, data: blog });
 });
 
 exports.editBlog = asyncHandler(async (req, res, next) => {
