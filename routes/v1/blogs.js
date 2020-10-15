f319b8da6350fa922130309bf1dd01a29a1c255e
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-15 10:44:24 +0530

    add advanced results middleware

--- routes/v1/blogs.js
+++ routes/v1/blogs.js
@@ -8,8 +8,10 @@ const {
   editBlog,
   deleteBlog
 } = require('../../controllers/blogs');
+const Blog = require('../../models/Blog');
+const advancedResults = require('../../middlewares/advancedResults');
 
-router.route('/').get(getBlogs).post(createBlog);
+router.route('/').get(advancedResults(Blog), getBlogs).post(createBlog);
 router.route('/featured').get(getFeaturedBlogs);
 router.route('/:id').get(getBlog).put(editBlog).delete(deleteBlog);
 
