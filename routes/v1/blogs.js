3da254634cae6502daff7b1ef05606adb2b5211c
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-15 10:34:16 +0530

    create blogs route

---
+++ routes/v1/blogs.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getBlog,
+  getBlogs,
+  getFeaturedBlogs,
+  createBlog,
+  editBlog,
+  deleteBlog
+} = require('../../controllers/blogs');
+
+router.route('/').get(getBlogs).post(createBlog);
+router.route('/featured').get(getFeaturedBlogs);
+router.route('/blog/:id').get(getBlog).put(editBlog).delete(deleteBlog);
+
+module.exports = router;
