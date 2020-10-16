1eed8a9ca9fde38d8abbfc656100f713210acef6
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-10-16 18:01:56 +0530

    update db url

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  let MONGO_URI = process.env.DB_DEV_URI;
-  if (process.env.NODE_ENV === 'production') {
-    MONGO_URI = process.env.DB_URI;
-  }
+  let MONGO_URI = process.env.DB_URI;
 
   const conn = await mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
