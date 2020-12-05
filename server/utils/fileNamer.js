adc672258598081a586ee343aa14180579bdb937
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-12-06 02:38:46 +0530

    update file namer file

--- server/utils/fileNamer.js
+++ server/utils/fileNamer.js
@@ -3,11 +3,12 @@
  * @param {String} originalName Original name of the file
  * @returns {String} new name of the file
  */
-const getBlobName = (originalName) => {
+const getBlobName = (originalName, authorName) => {
   // Use a random number to generate a unique file name,
   // removing "0." from the start of the string.
   const identifier = Math.random().toString().replace(/0\./, '');
-  return `${identifier}-${originalName}`;
+  authorName = authorName.replace(/\s/g, '');
+  return `${identifier}-${authorName}-${originalName}-`;
 };
 
 module.exports = getBlobName;
